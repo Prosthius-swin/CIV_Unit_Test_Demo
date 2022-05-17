02c6bc7556461284e5979855a978bbc08080ef55
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-17 15:12:43 +1000

    Completed

--- UnitTestDemo/Bollocks.cs
+++ UnitTestDemo/Bollocks.cs
@@ -8,25 +8,25 @@ namespace UnitTestDemo
         public string bollString { get; set; }
 
         // assigns values to properties
-        public Bollocks(int bollNum, string bollString){
-            throw new NotImplementedException("Bollocks.Constructor");
-        }
+        public Bollocks(int pBollNum, string pBollString){
+            bollNum = pBollNum;
+            bollString = pBollString;        }
 
         // method that returns bollNum summed with the parameter value
         public int AddTo(int n){
-            throw new NotImplementedException("Bollocks.AddTo");
-        } 
+            return n + bollNum;
+        }
  
         // method that DIVIDES bollNum BY 
         // the parameter value and returns the dividend
 
         public int DivBy(int n){
-            throw new NotImplementedException("Bollocks.DivBy");
+            return bollNum / n;
         }
         // method that returns the modulud of dividing bollNum BY 
         // the parameter value
         public int ModOfDivBy(int n){
-            throw new NotImplementedException("Bollocks.ModOfDivBy");
+            return bollNum % n;
         }
     }
 }
